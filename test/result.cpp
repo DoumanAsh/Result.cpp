633444be50db7ff1d2a422c2d8263cf669cc7104
Douman <douman@gmx.se>
2017-12-01 19:07:49 +0300

    Use catch2 for testing

--- test/result.cpp
+++ test/result.cpp
@@ -1,3 +1,4 @@
+#include <catch.hpp>
 
 #include <vector>
 #include <iostream>
@@ -6,15 +7,15 @@
 
 #include <result.hpp>
 
-void try1() {
+TEST_CASE("try result") {
     const auto ok = result::Result<int, std::string>::ok(1);
     const auto error = result::Result<int, std::string>::error("lolka");
 
-    assert(ok.is_ok());
-    assert(!ok.is_err());
+    REQUIRE(ok.is_ok());
+    REQUIRE_FALSE(ok.is_err());
 
-    assert(!error.is_ok());
-    assert(error.is_err());
+    REQUIRE_FALSE(error.is_ok());
+    REQUIRE(error.is_err());
 
     const auto ok_value = ok.unwrap();
     const auto error_value = error.unwrap_err();
@@ -22,14 +23,14 @@ void try1() {
     const auto ok_other_value = ok.unwrap_or(3);
     const auto other_value = error.unwrap_or(2);
 
-    assert(ok_value == 1);
-    assert(error_value == "lolka");
-    assert(other_value == 2);
-    assert(ok_other_value == 1);
+    REQUIRE(ok_value == 1);
+    REQUIRE(error_value == "lolka");
+    REQUIRE(other_value == 2);
+    REQUIRE(ok_other_value == 1);
 
 }
 
-void try2() {
+TEST_CASE("try result 2") {
     typedef std::vector<int> Vec;
 
     const auto ok = result::Result<Vec, std::string>::ok(Vec({1, 2, 3, 4, 5}));
@@ -37,8 +38,9 @@ void try2() {
     const auto ok_value = ok.unwrap();
     const auto ok_other_value = ok.unwrap_or(Vec(1, 5));
 
-    assert(ok_value == Vec({1, 2, 3, 4, 5}));
-    assert(ok_other_value == Vec({1, 2, 3, 4, 5}));
+    REQUIRE(ok_value == Vec({1, 2, 3, 4, 5}));
+    REQUIRE(ok_other_value == Vec({1, 2, 3, 4, 5}));
+
 }
 
 decltype(auto) return_pod_res_with_move() {
@@ -51,15 +53,12 @@ decltype(auto) return_non_pod_res_with_move() {
     return ok;
 }
 
-int main() {
-    try1();
-    try2();
+TEST_CASE("try return result") {
     auto pod_moved = return_pod_res_with_move();
-    assert(pod_moved.is_ok());
-    assert(pod_moved.unwrap() == 1);
+    REQUIRE(pod_moved.is_ok());
+    REQUIRE(pod_moved.unwrap() == 1);
     auto non_pod_moved = return_non_pod_res_with_move();
-    assert(non_pod_moved.is_ok());
-    assert(non_pod_moved.unwrap() == std::vector<int>(1, 2));
+    REQUIRE(non_pod_moved.is_ok());
+    REQUIRE(non_pod_moved.unwrap() == std::vector<int>(1, 2));
 
-    std::cout << "All ok, onii-chan!\n";
 }
