df93c4358e08d88009f218b10a26be45f586a08b
Douman <douman@gmx.se>
2018-07-11 22:19:17 +0300

    Clean-up cmake modules

--- .cmake/Modules/msvc.cmake
+++ .cmake/Modules/msvc.cmake
@@ -1,15 +1,3 @@
-# Statically links VC++ runtime
-macro(msvc_static_crt)
-    foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-        if(${flag_var} MATCHES "/MD")
-            string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-        endif(${flag_var} MATCHES "/MD")
-        if(${flag_var} MATCHES "/MDd")
-            string(REGEX REPLACE "/MDd" "/MTd" ${flag_var} "${${flag_var}}")
-        endif(${flag_var} MATCHES "/MDd")
-    endforeach(flag_var)
-endmacro(msvc_static_crt)
-
 # Set MVSC flags
 macro(msvc_set_flags_if)
     if(MSVC)
