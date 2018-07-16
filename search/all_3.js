b903b45479cee0faec20831bd4c9c6e6d68298db
Douman <douman@gmx.se>
2018-07-16 16:18:54 +0000

    Deploy DoumanAsh/Result.cpp to github.com/DoumanAsh/Result.cpp.git:gh-pages

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ok',['ok',['../classresult_1_1_result.html#a20cfc079199f09c445840c7bde1198d3',1,'result::Result::ok(T &amp;&amp;... value) noexcept(storage::is_value_noexcept)'],['../classresult_1_1_result.html#a1cc9bba94ca5853e7557d5ec0967d945',1,'result::Result::Ok()']]],
+  ['ok',['Ok',['../classresult_1_1_result.html#a1cc9bba94ca5853e7557d5ec0967d945',1,'result::Result::Ok()'],['../classresult_1_1_result.html#a20cfc079199f09c445840c7bde1198d3',1,'result::Result::ok(T &amp;&amp;... value) noexcept(storage::is_value_noexcept)']]],
   ['operator_20bool',['operator bool',['../classresult_1_1_result.html#ab4349fa16120820b925f3010c371470d',1,'result::Result']]],
   ['operator_3d',['operator=',['../classresult_1_1_result.html#a6f4b0341648ec4592a73f2bdfbce3291',1,'result::Result']]]
 ];
