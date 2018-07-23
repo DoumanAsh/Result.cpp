aeca5f470df31cfdb96c05786c020047871dab90
Douman <douman@gmx.se>
2018-07-23 04:49:25 +0000

    Deploy DoumanAsh/Result.cpp to github.com/DoumanAsh/Result.cpp.git:gh-pages

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value',['value',['../classresult_1_1_result.html#a6e81c9fdd389685aa8478bcc7c2e5a73',1,'result::Result::value()'],['../classresult_1_1_result.html#a0c60bbe1953a4148caccdd77577f0511',1,'result::Result::value() const']]]
+  ['value',['value',['../classresult_1_1_result.html#a3b052f842a6434d363237be21b48e323',1,'result::Result::value() noexcept'],['../classresult_1_1_result.html#a15efa6d2200dcff9b89b80b2216ef8bc',1,'result::Result::value() const noexcept']]]
 ];
